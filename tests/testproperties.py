3197b1889d81bffa4715a6309ec2bc631b357af4
Mats Taraldsvik <mats.taraldsvik@gmail.com>
2009-02-13 23:36:45 +0100

    Add database test directory. Black Books, Spaced, CSI ( two seasons ).

--- tests/testproperties.py
+++ tests/testproperties.py
@@ -54,6 +54,128 @@ testFiletypesContent = r'''<?xml version="1.0" encoding="UTF-8"?>
 </filetypes>
 '''
 
+# CONTENTS FOR DATABASE
+
+testDatabaseDirectory = os.path.join(testFileDirectory, 'database')
+testDatabase = { testDatabaseBlackBooks : 'blackbooks.xml', testDatabaseSpaced : 'spaced.xml', testDatabaseCSI : 'csi.xml' }
+
+testDatabaseBlackBooks = r'''<?xml version="1.0" encoding="UTF-8"?>
+<tvshow>
+  <showproperties backend="imdbtvbackend" duration="30" filesystem="ext3" name="Black Books" url="tt0262150"/>
+  <fileproperties>
+    <alias value="black.books"/>
+    <alias value="bb"/>
+  </fileproperties>
+  <season number="1">
+    <episode airdate="29 September 2000" arc="none" number="1" title="Cooking the Books"/>
+    <episode airdate="6 October 2000" arc="none" number="2" title="Manny's First Day"/>
+    <episode airdate="13 October 2000" arc="none" number="3" title="The Grapes of Wrath"/>
+    <episode airdate="20 October 2000" arc="none" number="4" title="The Blackout"/>
+    <episode airdate="27 October 2000" arc="none" number="5" title="The Big Lock-Out"/>
+    <episode airdate="3 November 2000" arc="none" number="6" title="He's Leaving Home"/>
+  </season>
+  <season number="2">
+    <episode airdate="1 March 2002" arc="none" number="1" title="The Entertainer"/>
+    <episode airdate="8 March 2002" arc="none" number="2" title="Fever"/>
+    <episode airdate="15 March 2002" arc="none" number="3" title="The Fixer"/>
+    <episode airdate="22 March 2002" arc="none" number="4" title="Blood"/>
+    <episode airdate="29 March 2002" arc="none" number="5" title="Hello Sun"/>
+    <episode airdate="5 April 2002" arc="none" number="6" title="A Nice Change"/>
+  </season>
+  <season number="3">
+    <episode airdate="11 March 2004" arc="none" number="1" title="Manny Come Home"/>
+    <episode airdate="18 March 2004" arc="none" number="2" title="Elephants and Hens"/>
+    <episode airdate="25 March 2004" arc="none" number="3" title="Moo-Ma and Moo-Pa"/>
+    <episode airdate="1 April 2004" arc="none" number="4" title="A Little Flutter"/>
+    <episode airdate="8 April 2004" arc="none" number="5" title="The Travel Writer"/>
+    <episode airdate="15 April 2004" arc="none" number="6" title="Party"/>
+  </season>
+</tvshow>
+'''
+
+testDatabaseSpaced = r'''<?xml version="1.0" encoding="UTF-8"?>
+<tvshow>
+  <showproperties backend="imdbtvbackend" duration="60" filesystem="ext3" name="Spaced" url="tt0187664"/>
+  <fileproperties/>
+  <season number="1">
+    <episode airdate="24 September 1999" arc="none" number="1" title="Beginnings"/>
+    <episode airdate="1 October 1999" arc="none" number="2" title="Gatherings"/>
+    <episode airdate="8 October 1999" arc="none" number="3" title="Art"/>
+    <episode airdate="15 October 1999" arc="none" number="4" title="Battles"/>
+    <episode airdate="22 October 1999" arc="none" number="5" title="Chaos"/>
+    <episode airdate="29 October 1999" arc="none" number="6" title="Epiphanies"/>
+    <episode airdate="5 November 1999" arc="none" number="7" title="Ends"/>
+  </season>
+  <season number="2">
+    <episode airdate="23 February 2001" arc="none" number="1" title="Back"/>
+    <episode airdate="2 March 2001" arc="none" number="2" title="Change"/>
+    <episode airdate="9 March 2001" arc="none" number="3" title="Mettle"/>
+    <episode airdate="23 March 2001" arc="none" number="4" title="Help"/>
+    <episode airdate="None" arc="none" number="5" title="Gone"/>
+    <episode airdate="6 April 2001" arc="none" number="6" title="Dissolution"/>
+    <episode airdate="13 April 2001" arc="none" number="7" title="Leaves"/>
+  </season>
+</tvshow>'''
+
+testDatabaseCSI = r'''<?xml version="1.0" encoding="UTF-8"?>
+<tvshow>
+  <showproperties backend="imdbtvbackend" duration="60" filesystem="ext3" name="C.S.I" url="tt0247082"/>
+  <fileproperties>
+    <alias value="csi"/>
+  </fileproperties>
+  <season number="1">
+    <episode airdate="6 October 2000" arc="none" number="1" title="Pilot"/>
+    <episode airdate="13 October 2000" arc="none" number="2" title="Cool Change"/>
+    <episode airdate="20 October 2000" arc="none" number="3" title="Crate 'n' Burial"/>
+    <episode airdate="27 October 2000" arc="none" number="4" title="Pledging Mr. Johnson"/>
+    <episode airdate="3 November 2000" arc="none" number="5" title="Friends &amp;#38; Lovers"/>
+    <episode airdate="10 November 2000" arc="none" number="6" title="Who Are You?"/>
+    <episode airdate="17 November 2000" arc="none" number="7" title="Blood Drops"/>
+    <episode airdate="24 November 2000" arc="none" number="8" title="Anonymous"/>
+    <episode airdate="8 December 2000" arc="none" number="9" title="Unfriendly Skies"/>
+    <episode airdate="22 December 2000" arc="none" number="10" title="Sex, Lies and Larvae"/>
+    <episode airdate="12 January 2001" arc="none" number="11" title="I-15 Murders"/>
+    <episode airdate="1 February 2001" arc="none" number="12" title="Fahrenheit 932"/>
+    <episode airdate="8 February 2001" arc="none" number="13" title="Boom"/>
+    <episode airdate="15 February 2001" arc="none" number="14" title="To Halve and to Hold"/>
+    <episode airdate="22 February 2001" arc="none" number="15" title="Table Stakes"/>
+    <episode airdate="1 March 2001" arc="none" number="16" title="Too Tough to Die"/>
+    <episode airdate="8 March 2001" arc="none" number="17" title="Face Lift"/>
+    <episode airdate="29 March 2001" arc="none" number="18" title="$35K O.B.O."/>
+    <episode airdate="12 April 2001" arc="none" number="19" title="Gentle, Gentle"/>
+    <episode airdate="19 April 2001" arc="none" number="20" title="Sounds of Silence"/>
+    <episode airdate="26 April 2001" arc="none" number="21" title="Justice Is Served"/>
+    <episode airdate="10 May 2001" arc="none" number="22" title="Evaluation Day"/>
+    <episode airdate="17 May 2001" arc="none" number="23" title="The Strip Strangler"/>
+  </season>
+  <season number="2">
+    <episode airdate="27 September 2001" arc="none" number="1" title="Burked"/>
+    <episode airdate="4 October 2001" arc="none" number="2" title="Chaos Theory"/>
+    <episode airdate="11 October 2001" arc="none" number="3" title="Overload"/>
+    <episode airdate="18 October 2001" arc="none" number="4" title="Bully for You"/>
+    <episode airdate="25 October 2001" arc="none" number="5" title="Scuba Doobie-Doo"/>
+    <episode airdate="1 November 2001" arc="none" number="6" title="Alter Boys"/>
+    <episode airdate="8 November 2001" arc="none" number="7" title="Caged"/>
+    <episode airdate="15 November 2001" arc="none" number="8" title="Slaves of Las Vegas"/>
+    <episode airdate="22 November 2001" arc="none" number="9" title="And Then There Were None"/>
+    <episode airdate="6 December 2001" arc="none" number="10" title="Ellie"/>
+    <episode airdate="13 December 2001" arc="none" number="11" title="Organ Grinder"/>
+    <episode airdate="20 December 2001" arc="none" number="12" title="You've Got Male"/>
+    <episode airdate="17 January 2002" arc="none" number="13" title="Identity Crisis"/>
+    <episode airdate="None" arc="none" number="14" title="The Finger"/>
+    <episode airdate="7 February 2002" arc="none" number="15" title="Burden of Proof"/>
+    <episode airdate="28 February 2002" arc="none" number="16" title="Primum Non Nocere"/>
+    <episode airdate="7 March 2002" arc="none" number="17" title="Felonious Monk"/>
+    <episode airdate="28 March 2002" arc="none" number="18" title="Chasing the Bus"/>
+    <episode airdate="4 April 2002" arc="none" number="19" title="Stalker"/>
+    <episode airdate="25 April 2002" arc="none" number="20" title="Cats in the Cradle..."/>
+    <episode airdate="2 May 2002" arc="none" number="21" title="Anatomy of a Lye"/>
+    <episode airdate="9 May 2002" arc="none" number="22" title="Cross-Jurisdictions"/>
+    <episode airdate="18 May 2002" arc="none" number="23" title="The Hunger Artist"/>
+  </season>
+</tvshow>
+'''
+
 # CREATE FILE NAMES FOR TESTING
 #TODO: Create checks if directories exist.
 import os
@@ -67,6 +189,9 @@ class Tools :
         self.testFiles = testFileNames
         
         self.filetypesXML = os.path.join(testFiletypesDirectory, 'filetypes.xml')
+        
+        self.databaseDir = testDatabaseDirectory
+        
     
     def createRootDir(self):
         if os.path.exists(self.rootDir) :
@@ -89,9 +214,15 @@ class Tools :
         shutil.rmtree(self.rootDir)
     
     def createFilesystemXML(self):
-        filesystemsXML = open(self.filetypesXML ,"w")
-        filesystemsXML.writelines(testFiletypesContent)
-        filesystemsXML.close()
+        testfile = open(self.filetypesXML ,"w")
+        testfile.writelines(testFiletypesContent)
+        testfile.close()
+    
+    def createDatabaseFiles(self):
+        for content, filename in testDatabase.items() :
+            testfile = open(os.path.join( self.databaseDir, filename ),"w")
+            testfile.writelines( content )
+            testfile.close()
 
 # TEST CLASS
 
